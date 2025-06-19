17f3fc758364c33d0931710cf8ad0a59e0a538ec
ASUS <mustafasharaf100@gmail.com>
2025-06-19 18:51:53 +0300

    update main

--- Program.cs
+++ Program.cs
@@ -15,25 +15,7 @@ namespace FilesCompressionProject
         static void Main()
         {
 
-            int[] x = { 5, 5, 2, 1, 1, 1, 1, 3 };
-            List<int> y = new List<int>();
-            int c = 1;
-            for (int i = 0; i < x.Length - 1; i++)
-            {
-                if (x[i] == x[i + 1])
-                {
-                    c++;
-                }
-                else
-                {
-                    y.Add(c);
-                    y.Add(x[i]);
-                    c = 1;
-                }
-            }
-            y.Add(c);
-            y.Add(x[x.Length - 1]);
-            Console.WriteLine(string.Join(", ", y));
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
